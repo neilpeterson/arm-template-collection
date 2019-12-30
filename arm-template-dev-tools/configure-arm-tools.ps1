cc65d0acf0ecc4e5e8cee7a1d228a4d3c79045cc
neilpeterson <neilpeterson@outlook.com>
2019-12-30 12:24:06 -0800

    script update

--- arm-template-dev-tools/configure-arm-tools.ps1
+++ arm-template-dev-tools/configure-arm-tools.ps1
@@ -12,7 +12,7 @@
 
 <#
 .SYNOPSIS
-    A super simple script to install VS Code during an Azure VM Deployment. 
+    A super simple script to install VS Code and the ARM Template tools during an Azure VM Deployment. 
     For a more complete / customizable installaton script see:
     https://github.com/PowerShell/vscode-powershell/blob/master/scripts/Install-VSCode.ps1
 #>
@@ -21,7 +21,7 @@ New-Item -ItemType Directory c:\temp\
 
 Invoke-WebRequest https://vscode-update.azurewebsites.net/latest/win32-x64/stable -OutFile c:\temp\vscode.exe
 
-Start-Process c:\temp\vscode.exe -ArgumentList /silent -Wait
+Start-Process c:\temp\vscode.exe -ArgumentList /silent
 
 Invoke-WebRequest https://github.com/microsoft/vscode-azurearmtools/releases/download/v0.8.3/azurerm-vscode-tools-0.8.3.vsix -OutFile c:\temp\azurerm-vscode-tools-0.8.3.vsix
 
